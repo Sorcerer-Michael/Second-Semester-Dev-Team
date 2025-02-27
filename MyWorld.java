8b5fffe956ae724761e2efaf7dda16f9ce5ade01
Etheiry <lateluffy@gmail.com>
2025-02-27 16:20:32 -0500

    Initial Setup

--- MyWorld.java
+++ MyWorld.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
+
 /**
  * Write a description of class MyWorld here.
  * 
@@ -8,14 +9,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    // Player object
+    Player player = new Player();
+    
+    // assuming world dimensions wont change after starting : change if needed to regular int variable
+    final int WORLDWIDTH = getWidth();
+    final int WORLDHEIGHT = getHeight();
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
-    {    
+    {   
+        
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        
+        // background image setup
+        GreenfootImage bg = new GreenfootImage("Placeholder", 60, Color.BLACK, Color.GRAY);
+        bg.scale(WORLDWIDTH, WORLDHEIGHT);
+        setBackground(bg);
+        
+        
+        // Add Actors 
+        addObject(player, WORLDWIDTH/2, WORLDHEIGHT/2);
+        
     }
 }
---
+++ Player.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player extends Actor
+{
+    public Player () {
+        
+    }
+    /**
+     * Act - do whatever the Player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        movement();
+    }
+    
+    public void movement() {
+        // turn(int amount) : turns actor in degrees
+        // turnTowards(int x, int y) : Turns this actor to face towards a certain location
+        // setLocation(int x, int y) : assign a new location to this actor
+        // setRotation(int rotation) : set the rotation of this actor
+    }
+}
