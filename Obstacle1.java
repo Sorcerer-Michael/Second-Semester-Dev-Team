81e7b903762abc7b5cd08eb585ef4a0d07648aa4
Ralph Reyes <ralphreyes02@icloud.com>
2025-03-08 14:02:56 -0500

    Testing for Obstacle1
    Adding Obstacle1
    Testing for Placement - Unresolved

--- Obstacle1.java
+++ Obstacle1.java
@@ -8,12 +8,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Obstacle1 extends Actor
 {
+    private int scrollspeed;    
+    
     /**
      * Act - do whatever the Obstacle1 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+       checkCollision();
+    }
+    
+    public Obstacle1(int scrollspeed){
+        this.scrollspeed = scrollspeed;
+    }
+    
+    private void InPosition(){
+        setLocation(getX(), getY() - scrollspeed);
+    }
+    
+    private void checkCollision(){
+        if (isTouching(Player.class)){
+            Greenfoot.stop();
+            System.out.println("You Crashed");
+        }
     }
 }
--- RoadWorld.java
+++ RoadWorld.java
@@ -1,5 +1,5 @@
 import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
- 
+
 /**
  * Write a description of class MyWorld here.
  *
@@ -10,14 +10,15 @@ public class RoadWorld extends World
 {
     int WORLDWIDTH = getWidth();
     int WORLDHEIGHT = getHeight();
- 
+
     private GreenfootImage backgroundImage;
     private int scrollY;
     private boolean scrollComplete = false;
- 
+    public int scrollspeed = 4;
+
     Player player = new Player(); // Player object
-    Obstacle1 obstacle1 = new Obstacle1(); // Add Obstacle1
- 
+    Obstacle1 obstacle1 = new Obstacle1(scrollspeed); // Add Obstacle1
+
     /**
      * Constructor for objects of class MyWorld.
      *
@@ -25,29 +26,32 @@ public class RoadWorld extends World
     public RoadWorld()
     {
         super(500, 600, 1);
- 
+
         backgroundImage = new GreenfootImage("road.png");
         scrollY = -backgroundImage.getHeight() + getHeight(); // Bottom Y-Coordinate
- 
+
+        setBackground(backgroundImage);
+
         // Add Actors
         addObject(player, WORLDWIDTH/2, WORLDHEIGHT-50);
-        addObject(obstacle1, 0, 15);
+
+        addObject(obstacle1, 250, 300);
     }
- 
+
     public void act() {
         checkScrolling();
-    }
- 
+        }
+
     private void scrollBackground(){
         if (scrollY < 0) {
             GreenfootImage background = getBackground();
             background.drawImage(backgroundImage, 0, scrollY);
-            scrollY += 4; // Adds 2 pixels to ScrollY
+            scrollY += scrollspeed; // Adds 4 pixels to ScrollY
         } else {
             scrollComplete = true;
         }
     }
-    
+
     private void checkScrolling(){
         if (!scrollComplete){
             scrollBackground();
