929aa794bef5f0cced304fab974d15108fd2bd1e
Ralph Reyes <ralphreyes02@icloud.com>
2025-03-08 14:23:02 -0500

    Added Obstacle Removal
    Testing for Obstacles spawning and despawning

--- Obstacle1.java
+++ Obstacle1.java
@@ -9,28 +9,44 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Obstacle1 extends Actor
 {
     private int scrollspeed;    
-    
+
     /**
      * Act - do whatever the Obstacle1 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-       checkCollision();
+        checkCollision();
+        InPosition();
     }
-    
+
+    /**
+     * Constructor for Obstacle1; Takes Scroll Speed as Input in RoadWorld;
+     */
     public Obstacle1(int scrollspeed){
         this.scrollspeed = scrollspeed;
     }
-    
+
     private void InPosition(){
-        setLocation(getX(), getY() - scrollspeed);
+        setLocation(getX(), getY() + scrollspeed);
+        if (getY() >= getWorld().getHeight()){ //If obstacle touches the bottom screen
+            getWorld().removeObject(this); 
+            addNewObstacle();
+        }
     }
-    
+
     private void checkCollision(){
-        if (isTouching(Player.class)){
+        if (isTouching(Player.class)){ //Player collides with Obstacle
             Greenfoot.stop();
             System.out.println("You Crashed");
         }
+    }
+
+    private void addNewObstacle(){
+        RoadWorld world = (RoadWorld) getWorld();
+        int randomX = Greenfoot.getRandomNumber(world.getWidth());
+
+        Obstacle1 newObstacle = new Obstacle1(scrollspeed);
+        world.addObject(newObstacle, randomX, -100);
     }
 }
