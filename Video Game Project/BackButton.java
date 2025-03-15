a373de9b6d40a0d337682848b6908dd55426dda4
MichaelRobinso <michr2946@outlook.com>
2025-03-15 13:33:45 -0400

    BackButton & Character Selection
    Created back Button and character selection screen

--- Video Game Project/BackButton.java
+++ Video Game Project/BackButton.java
@@ -12,20 +12,37 @@ public class BackButton extends Actor
      * Act - do whatever the backButton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private GreenfootImage stage1;
+    private GreenfootImage stage2;
+    
+    private boolean counting;
+    private int counter;
+    
     public BackButton(){
-        setImage("backarrow1.png");
+        stage1 = new GreenfootImage("backarrow1.png");
+        stage2 = new GreenfootImage("backarrow2.png");
+        
+        stage1.scale(stage1.getWidth()*2, stage1.getHeight()*2);
+        stage2.scale(stage2.getWidth()*2, stage2.getHeight()*2);
+        
+        setImage(stage1);
+        
     }
     
     public void act()
     {
-        if(){
-            setImage("backarrow2.png");
-        }
-        else{
-            setImage("backarrow1.png");
+        if (counting){
+            counter++;
+            if (counter >= 10){
+                setImage(stage1);
+                counting = !counting;
+                Greenfoot.setWorld(new Menu());
+            }
         }
         if(Greenfoot.mouseClicked(this)){
-            Greenfoot.setWorld(new Menu());
+            setImage(stage2);
+            counting = true;
+            counter = 0;
         }
     } 
 }
--- Video Game Project/StartButton.java
+++ Video Game Project/StartButton.java
@@ -14,7 +14,7 @@ public class StartButton extends Actor
     public void act()
     {
         if (Greenfoot.mouseClicked(this)){
-            Greenfoot.setWorld(new GameWorld());
+            Greenfoot.setWorld(new CharacterSelection());
         }
     }
 }
