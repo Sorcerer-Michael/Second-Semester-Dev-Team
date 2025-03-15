f05706fcb913ff697459363c5273df33aca89ca4
Ralph Reyes <ralphreyes02@icloud.com>
2025-03-15 12:16:40 -0400

    Added Code for RestartButton
    Makes it so that you return back to the previous world, if clicked the button

--- RestartButton.java
+++ RestartButton.java
@@ -9,11 +9,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class RestartButton extends Actor
 {
     /**
+     * Sets the Image for restart button
+     */
+    public RestartButton(){
+        setImage("restartButton.png");
+    }
+    
+    
+    /**
      * Act - do whatever the RestartButton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        if (Greenfoot.mouseClicked(this)){
+            Greenfoot.setWorld(getWorld());
+        }
     }
 }
