05768b0cea1b46830ae34ebe81ba277d1cf6abb1
mrasamny <mrasamny@desu.edu>
2025-03-19 13:41:50 -0400

    massive conflict
    massive

--- GameOverWorld.java
+++ GameOverWorld.java
@@ -18,21 +18,7 @@ public class GameOverWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(500, 600, 1); 
         
-        GreenfootImage bg = new GreenfootImage(getWidth(), getHeight());
-        bg.setColor(new Color(128, 128, 128, 180));
-        bg.fillRect(0, 0, getWidth(), getHeight());
-        setBackground(bg);
         
-        GreenfootImage text = new GreenfootImage("GAME OVER", 80, Color.WHITE, new Color(0, 0, 0, 0));
-        getBackground().drawImage(text, 50, 100);
-        
-        RestartButton restartButton = new RestartButton();
-        addObject(restartButton, 125, 325);
-        
-        HomeButton homeButton = new HomeButton();
-        addObject(homeButton, 375, 325);
-        
-        // this is a comment
     }
     
     public void blowWorld(){
