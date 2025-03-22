596d18d332afc9588d8bf1f4a77ed1805cf8c9ae
Ralph Reyes <ralphreyes02@icloud.com>
2025-03-21 21:01:27 -0400

    Scrolling white inbound
    If the player is at boundary, the scrolling begins

--- RoadWorld.java
+++ RoadWorld.java
@@ -12,7 +12,7 @@ public class RoadWorld extends World
 {
     private SimpleTimer time = new SimpleTimer();
     private Counter timer = new Counter();
-    
+
     private final int WORLDWIDTH = getWidth();
     private final int WORLDHEIGHT = getHeight();
 
@@ -21,14 +21,13 @@ public class RoadWorld extends World
     private int backgroundY;
     private boolean scrollComplete = false;
     private int lastY;
- 
+
     private Player player = new Player("images/player1_side.png"); // Player object
 
     private final int TOP_BOUNDARY = 250;
     private final int BOTTOM_BOUNDARY = 400;
 
     private List<Actor> obstacles = new ArrayList<>();
-    
 
     /**
      * Constructor for objects of class MyWorld.
@@ -40,14 +39,14 @@ public class RoadWorld extends World
         Meth.collisionCount = 0; // band aid fix to resetting collision count
         backgroundImage = new GreenfootImage("road.png");
         backgroundY = -backgroundImage.getHeight() + getHeight(); // Bottom Y-Coordinate
-        
+
         Greenfoot.setSpeed(50);
-        
+
         timer.setValue(30);
         time.mark();
         showText("Timer", 40, 20);
         addObject(timer, 40, 50);
-        
+
         setBackground(backgroundImage);
 
         // add obstacles to obstacle manager (List) ! probably want to create a separate obstacle manager class w/ allows for more control of what level gets what enemies.
@@ -56,15 +55,15 @@ public class RoadWorld extends World
         for (int count = 0; count < 5; count++) {
             obstacles.add(new Meth(2));
         }
-        
+
         // Add Actors
         addObject(player, WORLDWIDTH/2, WORLDHEIGHT-50);
-        
+
         // loops through obstacle list and spawns them
         for (Actor obstacle : obstacles) {
             SpawnObstacle(obstacle);
         }
-        
+
         //System.out.println("Meth Collision Count: " + Meth.collisionCount);
     }
 
@@ -83,18 +82,28 @@ public class RoadWorld extends World
     private void scrollBackground(){
         int playerY = player.getY();
         int playerMovement = lastY - playerY;
-        
+
         // if background at top, scroll is done
         if (backgroundY >= 0){
             backgroundY = 0;
             scrollComplete = true;
         }
-        
+
         // if scroll not done, adjust background when player at boundary
         if (!scrollComplete && ((playerY <= TOP_BOUNDARY && playerMovement > 0) || (playerY >= BOTTOM_BOUNDARY && playerMovement <0))){
             backgroundY += playerMovement;
+            //keep player in boundary
+            if (playerY <= TOP_BOUNDARY && playerMovement > 0){
+                player.setLocation(player.getX(), TOP_BOUNDARY);
+            } else {
+                player.setLocation(player.getX(), BOTTOM_BOUNDARY);
+            }
         }
+        
+        // update background 
+        
     }
+
     private void checkScrolling(){
         if (!scrollComplete){
             scrollBackground();
@@ -104,13 +113,13 @@ public class RoadWorld extends World
     public boolean isScrollComplete(){
         return scrollComplete;
     }
-    
+
     private void SpawnObstacle(Actor obstacle){ //Random Spawn per Run
         int randomX = Greenfoot.getRandomNumber(getWidth());
         int randomY = Greenfoot.getRandomNumber(200);
         addObject(obstacle, randomX, randomY);
     }  
-    
+
     private void isGameFinished(){
         if (player.getY() <= 0 && scrollComplete){
             System.out.println("You Win");
